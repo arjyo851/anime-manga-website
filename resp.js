250adaf279d2b6f3d0d072f429ce29c19b67d8db
arjyo851 <arjyo77@gmail.com>
2021-08-20 20:41:47 +0530

    new changes

--- resp.js
+++ resp.js
@@ -8,3 +8,45 @@ burger.addEventListener('click',()=>{
       navList.classList.toggle('v-class-resp');
       navbar.classList.toggle('h-nav-resp');
 })
+
+function fillDetails(){
+  var name = document.getElementById("name").value;
+  var email;
+  var phone;
+  var text;
+  if (name === "Arjyo") {
+        email = "arjyo77@gmail.com";
+        phone = "9310304682";
+        text = "Thank you for writing my name";
+  }
+  else{
+        email = "";
+        phone = "";
+        text = "";
+  }
+  document.getElementById("email").value = email;
+  document.getElementById("phone").value = phone;
+  document.getElementById("text").value = text;
+}
+
+function checkForName(){
+      if(document.getElementById("name").value.length === 0){
+            alert("Please enter your name");
+            document.getElementById("name").focus();
+            return false;
+      }
+      else if(document.getElementById("phone").value.length === 0 ){
+            alert("Please enter your phone number");
+            document.getElementById("phone").focus();
+            return false;
+      }
+      else if (document.getElementById("phone").value.length > 13) {
+            alert("Check your phone number");
+            return false;
+      }
+      else if(document.getElementById("email").value.length === 0){
+            alert("Please enter your email");
+            document.getElementById("email").focus();
+            return false;
+      }
+}
